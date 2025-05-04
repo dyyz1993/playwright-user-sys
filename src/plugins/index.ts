6a2e8e2cd47d4702d4f8b6d17043bcc9905da48e
xuyingzhou <xuyingzhou@tuzhanai.com>
2025-05-04 23:50:32 +0800

    修复

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -67,7 +67,8 @@ export default fp(async function (fastify: FastifyInstance) {
     cookieName: 'sessionId',
     secret: config.jwt.secret,
     cookie: {
-      secure: process.env.NODE_ENV === 'production',
+      // secure: process.env.NODE_ENV === 'production',
+      secure: false,
       httpOnly: true,
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 天
     }

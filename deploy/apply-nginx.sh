2314bfc5c1db4a0ce2fe8a3577077254cfc4db47
xuyingzhou <xuyingzhou@tuzhanai.com>
2026-05-27 02:06:06 +0800

    chore: clean up redundant Docker and deploy configs
    - Remove root-level docker-compose.yml/example/full (kept in docker/)
    - Remove deploy/ dir (replaced by docker/scripts/)
    - Remove docker/ stale docs (CHANGELOG/DEPLOYMENT/README, replaced by website/)
    - Remove redundant docker/manager/Dockerfile + docker/machine/Dockerfile (kept .prod)
    - Remove docker-compose.dev.yml (unused)
    - Remove docker/.env.local (contained plaintext passwords)

--- deploy/apply-nginx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp /opt/playwright-user-sys/deploy/jianli-with-playwright.conf /etc/nginx/conf.d/jianli.conf
-nginx -t
-nginx -s reload
-echo "DONE"
--- deploy/update-nginx.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cp /opt/playwright-user-sys/deploy/nginx-playwright.conf /etc/nginx/sites-available/default
-nginx -t
-nginx -s reload
-echo "NGINX_OK"
